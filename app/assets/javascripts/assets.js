5b17749957dcdc6becb0655700516d77e3567019
Miha Rebernik <miha@rebernik.info>
2016-08-21 12:46:09 -0700

    WIP: JS struct

--- app/assets/javascripts/assets.js
+++ app/assets/javascripts/assets.js
@@ -6,15 +6,53 @@ $(function() {
   $('.assets').DataTable();
 
   $('.pick').on('change', function(e) {
-    if ($(this).get(0).checked) {
-      var input = $(this).next();
-      if (!input.val()) {
-        input.val(1);
-      }
-      input.show().focus();
+    var $this = $(this);
+    var asset_data = $this.parents('tr').data();
+
+    if ($this.get(0).checked) {
+      var asset = new Asset(asset_data);
+      assets.add_asset(asset);
     }
     else {
-      $(this).next().val('').hide();
+      var asset = assets.find_asset(asset_data.code);
+      assets.remove_asset(asset);
     }
   });
+
+  var assets = new Assets();
+
 });
+
+var Asset = function(options) {
+  this.id = options.id;
+  this.name = options.name;
+  this.code = options.code;
+}
+
+var Assets = function() {
+  this.assets = [];
+};
+Assets.prototype = {
+  add_asset: function(asset) {
+    this.assets.push(asset);
+    this.render();
+  },
+
+  remove_asset: function(asset) {
+    if (!asset) { return; }
+    var index = this.assets.indexOf(asset);
+    if (index < 0) { return; }
+
+    this.assets.splice(index, 1);
+    this.render();
+  },
+
+  find_asset: function(code) {
+    return this.assets.filter(function(asset) { return asset.code === code })[0];
+  },
+
+  render: function() {
+    console.log('render');
+    console.log(this.assets);
+  }
+};
